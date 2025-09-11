bd318804d579e1952dba1778c6d9cd2559007eab
Yixin <chenyixin1022@gmail.com>
2025-09-11 18:00:00 +0800

    Add files via upload

---
+++ Condition&Switch (Grade Level).cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("请输入成绩(0~100):");
+        string? line = Console.ReadLine();  //string?   可空字符串（nullable string）
+        if (!int.TryParse(line, out int score) || score < 0 || score > 100)
+        {
+            Console.WriteLine("输入无效");
+            return;
+        }
+
+        string level;
+        if (score >= 90)
+            level = "优秀";
+        else if (score >= 80) level = "良好";
+        else if (score >= 60) level = "及格";
+        else level = "不及格";
+        Console.WriteLine($"if-else判定：{level}");
+
+
+        string level2 = score switch
+        {
+            >= 90 => "优秀",
+            >= 80 => "良好",
+            >= 60 => "及格",
+            _ => "不及格"
+        };
+        Console.WriteLine($"switch 判定:{level2}");
+        
+
+    }
+
+}
+
