770f5245dd33032cfc7616810dc99c56cfa8e2c0
Yixin <chenyixin1022@gmail.com>
2025-09-11 17:19:20 +0800

    Add files via upload

---
+++ Variables and Interpolation.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 20;
+        double score = 92.5;
+        bool isStudent = true;
+        string name = "Jerry";
+
+        Console.WriteLine($"Name: {name}, Age: {age}, Score: {score}, IsStudent: {isStudent}");
+        
+    }
+}
