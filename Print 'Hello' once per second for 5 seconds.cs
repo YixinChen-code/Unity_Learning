934ad8d42daba0e70ad5d1a87b19446a0c27035c
Yixin <chenyixin1022@gmail.com>
2025-09-14 16:57:27 +0800

    Add files via upload

---
+++ Print 'Hello' once per second for 5 seconds.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static async Task HelloLoop()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Hello");
+            await Task.Delay(1000);
+        }
+    }
+
+    static async Task Main()
+    {
+        await HelloLoop();
+        Console.WriteLine("Done.");
+    }
+
+}
+
