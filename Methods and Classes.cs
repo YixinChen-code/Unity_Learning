0661cb1b348aa3a85bfe901085ba5468b0969890
Yixin <chenyixin1022@gmail.com>
2025-09-14 15:34:48 +0800

    Add files via upload

---
+++ Methods and Classes.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+
+class Person
+{
+    public string Name = "";
+    public int Age;
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, I am {Name}, {Age} years old.");
+    }
+}
+class Program
+{
+    static int Add(int a, int b) => a + b;
+    static void SayHello(string name) => Console.WriteLine($"Hello, {name}");
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"3+5={Add(3, 5)}");
+        SayHello("Jerry");
+
+        var p = new Person { Name = "Alice", Age = 22 };
+        p.Introduce();
+    }
+
+}
+
