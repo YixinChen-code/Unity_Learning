de15fa4377ed7a2445494d529ffd570da225aef5
Yixin <chenyixin1022@gmail.com>
2025-09-14 17:42:40 +0800

    Add files via upload

---
+++ Parallel execution of two tasks (summation&countdown).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task<int> Sum1To100()
+    {
+        return await Task.Run(() => Enumerable.Range(1, 100).Sum());
+    }
+
+    static async Task Countdown()
+    {
+        for (int i = 100; i >= 1; i--)
+        {
+            Console.Write($"{i} ");
+            await Task.Delay(10);
+        }
+        Console.WriteLine();
+    }
+
+    static async Task Main()
+    {
+        Task<int> sumTask = Sum1To100();
+        Task countdownTask = Countdown();
+
+        await Task.WhenAll(sumTask, countdownTask);
+        Console.WriteLine($"Sum(1..100)={sumTask.Result}");
+        
+    }
+
+
+
+}
+
