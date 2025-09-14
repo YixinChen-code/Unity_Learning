1df057e0ecedf926f469ee661a8a4189bbf3552c
Yixin <chenyixin1022@gmail.com>
2025-09-14 17:58:20 +0800

    Add files via upload

---
+++ Simple TCP Server (Printing Client Messages).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class SimpleTcpServer
+{
+    public static async Task Main()
+    {
+        var listener = new TcpListener(IPAddress.Loopback, 9000);
+        listener.Start();
+        Console.WriteLine("server listening on 127.0.0.1:9000");
+
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            _ = HandleClient(client);
+        }
+    }
+
+
+    static async Task HandleClient(TcpClient client)
+    {
+        Console.WriteLine("client connected.");
+        using var stream = client.GetStream();
+
+        var buffer = new byte[1024];
+        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+        string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+        Console.WriteLine($"received:{msg}");
+        client.Close();
+    }
+}
+
