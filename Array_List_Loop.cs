50f8f14024de0da569e088fbcf3399c0ecfda77b
Yixin <chenyixin1022@gmail.com>
2025-09-14 15:18:31 +0800

    Add files via upload

---
+++ Array_List_Loop.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        Console.WriteLine($"数组和= {sum}");
+        List<string> fruits = new List<string> { "apple", "banana" };
+        fruits.Add("orange");
+        fruits.Remove("banana");
+        bool hasApple = fruits.Contains("apple");
+
+        Console.WriteLine("fruits 列表: ");
+        foreach (var f in fruits)
+            Console.Write($"-{f}");
+        Console.WriteLine($"是否包含apple?{hasApple}");
+        
+    }
+
+}
+
