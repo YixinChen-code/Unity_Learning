60209f2b87f7ea91fbe67174f5ea2c3f70cdc418
Yixin <chenyixin1022@gmail.com>
2025-09-14 21:40:48 +0800

    Add files via upload

---
+++ Simple TCP client (sending Hello Server).cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class SimpleTcpClient
+{
+    public static async Task Main()
+    {
+        using var client = new TcpClient();
+        await client.ConnectAsync("127.0.0.1", 9000);
+        Console.WriteLine("Connected to server.");
+        var bytes = Encoding.UTF8.GetBytes("Hello Server\n");
+        await client.GetStream().WriteAsync(bytes, 0, bytes.Length);
+        Console.WriteLine("Sent.");
+    }
+}
